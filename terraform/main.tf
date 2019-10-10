80d3c9371d529800532bdaba6b4d0e929121a5b3
Alexander Peven <pevena@yandex.ru>
2019-10-10 14:04:40 +0300

    Add resource google_compute_instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,19 @@ provider "google" {
 
   region = "europe-west-1"
 }
+
+resource "google_compute_instance" "app" {
+  name = "reddit-app"
+  machine_type = "g1-small"
+  zone = "europe-west1-b"
+  boot_disk {
+    initialize_params {
+      image = " reddit-base-1570222252"
+    }
+  }
+  
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
