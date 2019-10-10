166ca721fae6b3f2050ea358bff825e2e333ce34
Alexander Peven <pevena@yandex.ru>
2019-10-10 12:10:19 +0300

    Terraform init

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  # Версия terraform
+  required_version = "0.12.8"
+}
+
+provider "google" {
+  # Версия провайдера
+  version = "2.15"
+
+  # ID проекта
+  project = "infra-253422"
+
+  region = "europe-west-1"
+}
