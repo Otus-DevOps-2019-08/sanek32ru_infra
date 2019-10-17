b2bb80a869329496d713adc4a272f3e63d145133
Alexander Peven <pevena@yandex.ru>
2019-10-17 17:20:38 +0300

    Split configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,3 +17,12 @@ variable private_key {
   description = "Private key"
 }
 
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
+}
