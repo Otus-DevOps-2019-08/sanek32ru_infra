4408aa515f9a71a64020ed213e40d1a5af3e5ca4
Alexander Peven <pevena@yandex.ru>
2019-10-18 15:55:08 +0300

    storage-bucket add location

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -7,9 +7,10 @@ provider "google" {
 module "storage-bucket" {
   source  = "SweetOps/storage-bucket/google"
   version = "0.3.0"
+  location = "europe-west1"
 
   # Имя поменяйте на другое
-  name = "storage-bucket-test"
+  name = "storage-bucket-i253422"
 }
 
 output storage-bucket_url {
