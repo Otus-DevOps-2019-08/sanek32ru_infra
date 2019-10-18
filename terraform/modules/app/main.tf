09c3f0ff10b0cfe4df6deae8533aad2258497597
Alexander Peven <pevena@yandex.ru>
2019-10-18 12:16:07 +0300

    Format text

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,8 +1,8 @@
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-  zone = var.zone
-  tags = ["reddit-app"]
+  zone         = var.zone
+  tags         = ["reddit-app"]
   boot_disk {
     initialize_params { image = var.app_disk_image }
   }
@@ -13,22 +13,21 @@ resource "google_compute_instance" "app" {
     }
   }
   metadata = {
+    # путь до публичного ключа
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
 }
-
-resource "google_compute_address" "app_ip" { 
+resource "google_compute_address" "app_ip" {
   name = "reddit-app-ip"
-  region = "europe-west1"
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
+  name    = "allow-puma-default"
   network = "default"
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports    = ["9292"]
   }
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["reddit-app"]
+  target_tags   = ["reddit-app"]
 }
