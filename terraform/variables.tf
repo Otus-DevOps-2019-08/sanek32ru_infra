0bb426cfdeb2c98cfd9f266156ad05cb88fe2502
Alexander Peven <pevena@yandex.ru>
2019-10-18 12:23:21 +0300

    Correct zone in main.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,6 +6,10 @@ variable region {
   # Значение по умолчанию
   default = "europe-west1"
 }
+variable zone {
+  description = "zone of VM dislocaton2"
+  default     = "europe-west1-b"
+}
 variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
